7ea4431cb52c7cff07ff480f4da472b75d87a86b
elinelton leite <elineltonleite@gmail.com>
2023-12-19 17:50:03 -0300

    crição de metodos dinamicos

--- sysBar/app/controllers/ControllerProdutos.php
+++ sysBar/app/controllers/ControllerProdutos.php
@@ -1,17 +1,38 @@
 <?php
 
-
 class ControllerProdutos{
 	
 	public function menuProdutos(){
 		$menu = New MenuProdutos();
 		$menu->montaMenu();
 	}
+	
+	
+	public function formCadastrarProduto(){
+		include_once'./app/views/formCadProduto.php';
+	}
+	
 	public function listaProdutosCadastrados(){
 		
 	}
+	public function teste(){echo'ok';}
+	
 }
 
+
+
+
 $p = New ControllerProdutos();
-$p->menuProdutos();
+//$p->menuProdutos();
+
+$p->formCadastrarProduto();
 $p->listaProdutosCadastrados();
+
+
+// chama o methodo dinamicamente
+
+if(isset($_REQUEST['m'])){
+	$m = $_REQUEST['m'];
+	$p->$m();
+}
+
--- sysBar/app/views/formCadProduto.php
+++ sysBar/app/views/formCadProduto.php
@@ -1,10 +1,16 @@
-<form>
-	<div>
-		<label for="">Descrição:</labeL>
-		<input type="text" id="">
+
+
+<form method="post"action="?link=app/controllers/ControllerProdutos&m=teste" id="formCadProd">
+	<h2>Cadastrar Produto</h2>
+	<div id="divDescricao">
+		<label for="txtDescricao">Descrição:</labeL>
+		<input type="text" id="txtDescricao" autofocus>
+	</div>
+	<div id="divPreco">
+		<label for="txtPreco">Preço:</labeL>
+		<input type="text" id="txtPreco">
 	</div>
 	<div>
-		<label for="">Preço:</labeL>
-		<input type="text" id="">
+		<button>Cadastrar</button>
 	</div>
 </form>
